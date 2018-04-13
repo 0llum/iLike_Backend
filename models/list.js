349591a33a543c3cc8446dc2a5205a3cfc51d2ac
s48024 <s48024@beuth-hochschule.de>
2018-04-13 17:37:10 +0200

    added list model

---
+++ models/list.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from 'mongoose';
+
+const listItemSchema = new Schema({
+  name: { type: String, required: true },
+  image: { type: String },
+  color: { type: String },
+  count: { type: Number, default: 0, min: 0 },
+  picks: { type: Number, default: 0, min: 0 },
+});
+
+const listSchema = new Schema({
+  name: { type: String, required: true },
+  image: { type: String },
+  color: { type: String },
+  items: [listItemSchema],
+});
+
+export default mongoose.model('list', listSchema);
