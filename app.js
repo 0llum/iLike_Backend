f0d47bdf715e7e2767331f433f0bca662b612e97
s48024 <s48024@beuth-hochschule.de>
2018-04-14 12:47:53 +0200

    patch test

--- app.js
+++ app.js
@@ -66,65 +66,20 @@ app.get('/lists/:id/:itemId', (req, res) => {
 });
 
 app.patch('/lists/:id/:itemId', (req, res) => {
-  ListModel.findOneAndUpdate({_id: req.params.id, "items._id": req.params.itemId}, { "items.$.count": 100 }, (err, item) => {
+  ListModel.findOneAndUpdate({
+    _id: req.params.id,
+    "items._id": req.params.itemId
+  }, {
+    inc: {"items.$.count": 1}
+  }, (err, item) => {
     if (err) {
-      console.log(err);
       res.status(404);
       res.end();
     } else {
-      console.log(item);
       res.status(200);
       res.json(item);
     }
   });
-
-  // ListModel.findOneAndUpdate(
-  //   {_id: req.params.id, items._id: req.params.itemId},
-  //   {"$inc": {"count": 1}},
-  //   (err, item) => {
-  //     if (err) {
-  //       res.status(406);
-  //       res.json(err);
-  //     } else {
-  //       res.status(200);
-  //       res.json(item);
-  //     }
-  //   }
-  // );
-
-  // ListModel.findById(req.params.id, (err, data) => {
-  //   if (err) {
-  //     res.status(404);
-  //     res.json(err);
-  //   } else {
-  //     const list = new ListModel(data);
-  //     list.items.forEach(item => {
-  //       if (item._id == req.params.itemId) {
-  //         if (req.body.count) {
-  //           item.set({
-  //             count: item.count + 1
-  //           });
-  //         }
-  //         if (req.body.picks) {
-  //           item.set({
-  //             picks: item.picks + 1
-  //           });
-  //         }
-  //         list.save(err => {
-  //           if (err) {
-  //             res.status(406);
-  //             res.json(err);
-  //           } else {
-  //             res.status(200);
-  //             res.json(item);
-  //           }
-  //         });
-  //       }
-  //     });
-  //     //res.status(404);
-  //     //res.end();
-  //   }
-  // })
 });
 
 const server = app.listen(3000, () => {
