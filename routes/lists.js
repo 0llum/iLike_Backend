86a117e7172c77bf27377921833d598131d7117c
s48024 <s48024@beuth-hochschule.de>
2018-04-16 22:42:20 +0200

    use router and middleware

--- routes/lists.js
+++ routes/lists.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import List from './models/list';
+import List from '../models/list';
 
 mongoose.connect('mongodb://localhost:27017/iLike');
 const lists = express.Router();
