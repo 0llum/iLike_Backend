897e066a87dae9c3b16bf8bad4418047eabe3572
s48024 <s48024@beuth-hochschule.de>
2018-04-16 20:49:43 +0200

    refactor get list/item/match

---
+++ models/listItem.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from 'mongoose';
+import ListItemMatch from './listItemMatch';
+
+const listItemSchema = new Schema({
+  name: { type: String, required: true },
+  image: { type: String },
+  color: { type: String },
+  count: { type: Number, default: 0, min: 0 },
+  picks: { type: Number, default: 0, min: 0 },
+  matches: [ListItemMatch],
+});
+
+export default mongoose.model('listItem', listItemSchema);
---
+++ models/listItemMatch.js
@@ -0,0 +1,9 @@
+import mongoose, { Schema } from 'mongoose';
+
+const listItemMatchSchema = new Schema({
+  itemId: { type: String, required: true },
+  count: { type: Number, default: 0, min: 0 },
+  picks: { type: Number, default: 0, min: 0 },
+});
+
+export default mongoose.model('listItemMatch', listItemMatchSchema);
