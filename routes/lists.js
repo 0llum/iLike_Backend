bd2c35d6c8dbedb2ae603283891356aeb34a857d
s48024 <s48024@beuth-hochschule.de>
2018-04-20 21:47:24 +0200

    fixed patch match

--- routes/lists.js
+++ routes/lists.js
@@ -12,7 +12,7 @@ lists.route('/')
         return res.status(404).json(err);
       }
       data.forEach(element => {
-        count = 0;
+        element.count = 0;
         element.__v = 0;
       });
       const list = new List(data);
