7061225d28c642bc206b9af10c915562cda6a7d6
s48024 <s48024@beuth-hochschule.de>
2018-04-20 16:40:45 +0200

    fixed patch match

--- routes/lists.js
+++ routes/lists.js
@@ -58,13 +58,10 @@ lists.route('/:id/:itemId')
       const item = data.items.id(req.params.itemId);
       item.matches.forEach(element => {
         data.items.forEach(x => {
-          if (x.id == element.itemId) console.log('ohne ohne');
-          if (x.id == element.toObject().itemId) console.log('ohne mit');
-          if (x.toObject().id == element.itemId) console.log('mit ohne');
-          if (x.toObject().id == element.toObject().itemId) console.log('mit mit');
-          if (x._id == element.itemId) {
+          if (x.id == element.itemId) {
             element.name = x.name;
             element.image = x.image;
+            console.log(element);
           }
         })
       });
