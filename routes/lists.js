2d9c9cbfb420bc7affffb48f2f9025eda13dea9d
s48024 <s48024@beuth-hochschule.de>
2018-04-20 16:22:47 +0200

    fixed patch match

--- routes/lists.js
+++ routes/lists.js
@@ -58,8 +58,8 @@ lists.route('/:id/:itemId')
       const item = data.items.id(req.params.itemId);
       item.matches.forEach(element => {
         data.items.forEach(x => {
-          console.log(x._id, element.toObject().itemId);
           if (x._id == element.toObject().itemId) {
+            console.log(x._id, element.toObject().itemId);
             element.name = x.name;
             element.image = x.image;
           }
