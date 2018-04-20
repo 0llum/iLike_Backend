3ef9572c9bda0d7253bd817993bbdad1acc79b46
s48024 <s48024@beuth-hochschule.de>
2018-04-20 20:49:55 +0200

    fixed patch match

--- routes/lists.js
+++ routes/lists.js
@@ -66,7 +66,7 @@ lists.route('/:id/:itemId')
         }
         array.push(match);
       });
-      item.matches = array;
+      item.toObject().matches = array;
       console.log(array);
       if (!item) {
         return res.status(404).end();
