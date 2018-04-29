2ca588d9abba8e21f3d35a1b6866e242ad429172
s48024 <s48024@beuth-hochschule.de>
2018-04-29 14:10:55 +0200

    new patch test

--- routes/lists.js
+++ routes/lists.js
@@ -61,6 +61,7 @@ lists.route('/:id')
       const list = data;
       if (req.body.items) {
         req.body.items.forEach(el => {
+          console.log(el);
           let item = list.items.id(el.id);
           if (el.count) {
             item.count = item.count + 1;
@@ -69,7 +70,7 @@ lists.route('/:id')
             item.picks = item.picks + 1;
           }
           if (el.matches) {
-            console.log(el);
+            
           }
         });
       }
