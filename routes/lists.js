bdd30b778c63402ed216b41404aad210f4d23c7f
Oliver Dolgener <odo@codecookies.com>
2018-05-03 11:39:11 +0200

    adjust matches

--- routes/lists.js
+++ routes/lists.js
@@ -151,6 +151,7 @@ lists
         const ref = data.items.find(x => x.id == element.itemId);
         element.name = ref.name;
         element.image = ref.image;
+        console.log(element);
       });
       res.status(200).json(item);
     });
