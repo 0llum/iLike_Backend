995874f8108957cc7e0324656aa5cf535ef9e2a6
Oliver Dolgener <odo@codecookies.com>
2018-05-03 11:46:50 +0200

    adjust matches

--- routes/lists.js
+++ routes/lists.js
@@ -136,110 +136,39 @@ lists
     });
   });
 
-lists
-  .route('/:id/:itemId')
-  .get((req, res) => {
-    List.findById(req.params.id, (err, data) => {
-      if (err) {
-        return res.status(404).json(err);
-      }
-      const item = data.items.id(req.params.itemId);
-      if (!item) {
-        return res.status(404).end();
-      }
-      item.matches.forEach(element => {
-        const ref = data.items.find(x => x.id == element.itemId);
-        element.name = ref.name;
-        element.image = ref.image;
-      });
-      res.status(200).json(item);
+lists.route('/:id/:itemId').get((req, res) => {
+  List.findById(req.params.id, (err, data) => {
+    if (err) {
+      return res.status(404).json(err);
+    }
+    const item = data.items.id(req.params.itemId);
+    if (!item) {
+      return res.status(404).end();
+    }
+    item.matches.forEach(element => {
+      const ref = data.items.find(x => x.id == element.itemId);
+      element.name = ref.name;
+      element.image = ref.image;
     });
-  })
-  .patch((req, res) => {
-    req.body.count &&
-      List.findOneAndUpdate(
-        {
-          _id: req.params.id,
-          'items._id': req.params.itemId,
-        },
-        {
-          $inc: { 'items.$.count': 1 },
-        },
-        (err, item) => {
-          if (err) {
-            return res.status(404).json(err);
-          }
-        },
-      );
-
-    req.body.picks &&
-      List.findOneAndUpdate(
-        {
-          _id: req.params.id,
-          'items._id': req.params.itemId,
-        },
-        {
-          $inc: { 'items.$.picks': 1 },
-        },
-        (err, item) => {
-          if (err) {
-            return res.status(404).json(err);
-          }
-        },
-      );
-
-    res.status(200).json();
+    res.status(200).json(item);
   });
+});
 
-lists
-  .route('/:id/:itemId/:itemMatchId')
-  .get((req, res) => {
-    List.findById(req.params.id, (err, data) => {
-      if (err) {
-        return res.status(404).json(err);
-      }
-      const item = data.items.id(req.params.itemId);
-      if (!item) {
-        return res.status(404).end();
-      }
-      const match = item.matches.find(x => x.toObject().itemId == req.params.itemMatchId);
-      if (!match) {
-        return res.status(404).end();
-      }
-      res.status(200).json(match);
-    });
-  })
-  .patch((req, res) => {
-    List.findById(req.params.id, (err, data) => {
-      const item = data.items.find(x => x.id == req.params.itemId);
-      let match = item.matches.find(x => x.toObject().itemId == req.params.itemMatchId);
-      if (!match) {
-        match = {};
-        match.itemId = req.params.itemMatchId;
-        match.count = 0;
-        match.picks = 0;
-        if (req.body.count) {
-          match.count = match.count + 1;
-        }
-        if (req.body.picks) {
-          match.picks = match.picks + 1;
-        }
-        item.matches.push(match);
-      } else {
-        if (req.body.count) {
-          match.count = match.count ? match.count + 1 : 1;
-        }
-        if (req.body.picks) {
-          match.picks = match.picks ? match.picks + 1 : 1;
-        }
-      }
-      data.save(err => {
-        if (err) {
-          return res.status(404).json(err);
-        }
-        return res.status(200).json(match);
-      });
-    });
+lists.route('/:id/:itemId/:itemMatchId').get((req, res) => {
+  List.findById(req.params.id, (err, data) => {
+    if (err) {
+      return res.status(404).json(err);
+    }
+    const item = data.items.id(req.params.itemId);
+    if (!item) {
+      return res.status(404).end();
+    }
+    const match = item.matches.find(x => x.toObject().itemId == req.params.itemMatchId);
+    if (!match) {
+      return res.status(404).end();
+    }
+    res.status(200).json(match);
   });
+});
 
 export default lists;
