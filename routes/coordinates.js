c9a9f50aa688c4ba36e58b43090c3f71451a309e
s48024 <s48024@beuth-hochschule.de>
2018-07-16 18:31:48 +0200

    insert multiple coordinates

--- routes/coordinates.js
+++ routes/coordinates.js
@@ -16,18 +16,26 @@ coordinates
     });
   })
   .post((req, res) => {
-    const coordinate = new Coordinate(req.body);
-    coordinate.save(err => {
+    Coordinate.insertMany(req.body, (err, docs) => {
       if (err) {
         return res.status(406).json(err);
       }
-      Coordinate.findById(coordinate.id, (err, data) => {
-        if (err) {
-          return res.status(404).json(err);
-        }
-        res.status(201).json(data);
-      });
+      if (docs) {
+        res.status(201).json(docs);
+      }
     });
+    // const coordinate = new Coordinate(req.body);
+    // coordinate.save(err => {
+    //   if (err) {
+    //     return res.status(406).json(err);
+    //   }
+    //   Coordinate.findById(coordinate.id, (err, data) => {
+    //     if (err) {
+    //       return res.status(404).json(err);
+    //     }
+    //     res.status(201).json(data);
+    //   });
+    // });
   });
 
 export default coordinates;
