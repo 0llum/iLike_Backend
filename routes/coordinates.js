f5d0fad9a902a0e4ca960f5a07ffaec1855b50f6
Oliver Dolgener <oliver.dolgener.wst@codecookies.com>
2018-07-26 12:16:53 +0200

    add generateCoordinates

--- routes/coordinates.js
+++ routes/coordinates.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import Coordinate from '../models/coordinate';
+import * as EarthUtils from '../utils/EarthUtils';
 
 mongoose.connect('mongodb://localhost:27017/iLike');
 const coordinates = express.Router();
@@ -31,5 +32,33 @@ coordinates
   .get((req, res) => {
     
   });
+
+generateCoordinates = (lat) => {
+  if (lat < -89) {
+    return;
+  }
+
+  const coordinates = [];
+  for (let lon = 0; lon < 360; k += EarthUtils.gridDistanceAtLatitude(lat)) {
+    const latitude = EarthUtils.getRoundedLatitude(lat);
+    const longitude = lon > 180 ? EarthUtils.getRoundedLongitude(lon - 360, lat) : EarthUtils.getRoundedLongitude(lon, lat);
+    // if (!Object.is(longitude, -0)) {
+    coordinates.push({ latitude, longitude });
+    // }
+  }
+
+  fetch('https://api.0llum.de/coordinates', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(coordinates),
+  }).then((response) => {
+    if (response.status === 201) {
+      this.generateCoordinates(lat - EarthUtils.GRID_DISTANCE);
+    }
+  });
+}
 
 export default coordinates;
