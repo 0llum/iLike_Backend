470cdef50e239d200bd749c11ffab40ae1e582dd
Oliver Dolgener <oliver.dolgener.wst@codecookies.com>
2018-09-27 10:17:19 +0200

    feat(mysql): added connection to mysql

--- routes/mysql.js
+++ routes/mysql.js
@@ -5,14 +5,16 @@ import mysql from 'mysql';
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
+  password: 'Olli9989'
   database: 'whib',
 });
 
 connection.connect(err => {
   if (err) {
-    throw err;
+    console.log(err);
+  } else {
+    console.log('connected');
   }
-  console.log('connected');
 });
 
 const newMysql = express.Router();
