6dba6276b777cbb760fc56f29f397c5602a22d4f
s48024 <s48024@beuth-hochschule.de>
2018-09-27 21:41:50 +0200

    generate coordinate

--- routes/coordinates.js
+++ routes/coordinates.js
@@ -19,54 +19,54 @@ connection.connect(err => {
   }
   console.log('connected');
 
-  coordinates
-    .route('/')
-    .get((req, res) => {
-      connection.query('SELECT * FROM coordinates', (err, data) => {
-        if (err) {
-          return res.status(404).json(err);
-        }
-        res.status(200).json(data);
-      });
-    })
+  coordinates.route('/').get((req, res) => {
+    connection.query('SELECT * FROM coordinates', (err, data) => {
+      if (err) {
+        return res.status(404).json(err);
+      }
+      res.status(200).json(data);
+    });
+  });
 
   coordinates.route('/generate').get((req, res) => {
     generateCoordinates(90);
   });
+});
 
-  let generateCoordinates = function(lat) {
-    console.log(lat);
-    if (lat < 89) {
-      return;
-    }
+const generateCoordinates = function(lat) {
+  console.log(lat);
+  if (lat < 89) {
+    return;
+  }
 
-    const locations = [];
-    for (let lon = 6; lon < 15; lon += EarthUtils.gridDistanceAtLatitude(lat)) {
-      const latitude = EarthUtils.getRoundedLatitude(lat);
-      const longitude =
-        lon > 180
-          ? EarthUtils.getRoundedLongitude(lon - 360, lat)
-          : EarthUtils.getRoundedLongitude(lon, lat);
-      // if (!Object.is(longitude, -0)) {
-        locations.push(`POINT(${latitude}, ${longitude})`);
-      // }
-    }
+  const locations = [];
+  for (let lon = 6; lon < 15; lon += EarthUtils.gridDistanceAtLatitude(lat)) {
+    const latitude = EarthUtils.getRoundedLatitude(lat);
+    const longitude =
+      lon > 180
+        ? EarthUtils.getRoundedLongitude(lon - 360, lat)
+        : EarthUtils.getRoundedLongitude(lon, lat);
+    // if (!Object.is(longitude, -0)) {
+    locations.push(`POINT(${latitude}, ${longitude})`);
+    // }
+  }
 
-    connection.query(
-      'INSERT INTO user (email, password, username) VALUES (?)',
-      locations,
-      (err, data) => {
-        generateCoordinates(lat - Earth.GRID_DISTANCE);
-      });
-      // Coordinate.insertMany(coordinates, (err, docs) => {
-        // if (err) {
-        //   console.log(err);
-        // }
-        // if (docs) {
-        //   generateCoordinates(lat - Earth.GRID_DISTANCE);
-        // }
-    //   generateCoordinates(lat - Earth.GRID_DISTANCE);
-    // });
+  connection.query(
+    'INSERT INTO user (email, password, username) VALUES (?)',
+    locations,
+    (err, data) => {
+      generateCoordinates(lat - Earth.GRID_DISTANCE);
+    },
+  );
+  // Coordinate.insertMany(coordinates, (err, docs) => {
+  // if (err) {
+  //   console.log(err);
+  // }
+  // if (docs) {
+  //   generateCoordinates(lat - Earth.GRID_DISTANCE);
+  // }
+  //   generateCoordinates(lat - Earth.GRID_DISTANCE);
+  // });
   // };
 };
 
