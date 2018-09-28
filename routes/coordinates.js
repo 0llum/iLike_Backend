aabcbc588e7e93d8dfbd024caf37e5562558648c
s48024 <s48024@beuth-hochschule.de>
2018-09-28 22:09:12 +0200

    get location from userId

--- routes/coordinates.js
+++ routes/coordinates.js
@@ -33,16 +33,12 @@ connection.connect(err => {
   });
 
   coordinates.route('/:id').get((req, res) => {
-    connection.query(
-      'SELECT * FROM coordinates WHERE user_id = ?',
-      [req.params.id],
-      (err, data) => {
-        if (err) {
-          return res.status(404).json(err);
-        }
-        res.status(200).json(data);
-      },
-    );
+    connection.query('SELECT * FROM locations WHERE user_id = ?', [req.params.id], (err, data) => {
+      if (err) {
+        return res.status(404).json(err);
+      }
+      res.status(200).json(data);
+    });
   });
 
   coordinates.route('/transition/:oldId/:newId').get((req, res) => {
