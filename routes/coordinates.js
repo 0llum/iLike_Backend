53c182004912e01d5bc2c7783a2dd2b26fa21504
s48024 <s48024@beuth-hochschule.de>
2018-09-28 22:03:49 +0200

    insert old locations

--- routes/coordinates.js
+++ routes/coordinates.js
@@ -41,7 +41,12 @@ connection.connect(err => {
         return res.status(404).end();
       }
       const locations = data.locations;
-      const newLocations = locations.map(x => [1, x.latitude, x.longitude, x.timestamp]);
+      const newLocations = locations.map(x => [
+        req.params.newId,
+        x.latitude,
+        x.longitude,
+        x.timestamp,
+      ]);
 
       connection.query(
         'INSERT INTO locations (user_id, latitude, longitude, timestamp) VALUES ?',
