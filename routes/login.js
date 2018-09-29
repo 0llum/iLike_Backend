7c531a90f8cc873ab75cb810313d1454ab40c136
s48024 <s48024@beuth-hochschule.de>
2018-09-29 13:47:38 +0200

    handle disconnect

--- routes/login.js
+++ routes/login.js
@@ -2,42 +2,53 @@ import express from 'express';
 import mysql from 'mysql';
 import bcrypt from 'bcrypt';
 
+import Connection from '../constants/Connection';
+
 const SALT_WORK_FACTOR = 10;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Olli9989',
-  database: 'whib',
-});
-
 const login = express.Router();
+let connection;
 
-connection.connect(err => {
-  if (err) {
-    throw err;
-  }
-  console.log('login router connected');
+function handleDisconnect() {
+  connection = mysql.createConnection(Connection);
 
-  login.route('/').post((req, res) => {
-    connection.query('SELECT * FROM user WHERE email = ?', [req.body.email], (err, data) => {
+  connection.connect(err => {
+    if (err) {
+      console.log(err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log('login router connected');
+    }
+  });
+
+  connection.on('error', err => {
+    console.log(err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+login.route('/').post((req, res) => {
+  connection.query('SELECT * FROM user WHERE email = ?', [req.body.email], (err, data) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    if (data.length < 1) {
+      return res.status(404).end();
+    }
+
+    const user = data[0];
+    bcrypt.compare(req.body.password, user.password, function(err, isMatch) {
       if (err) {
-        return res.status(404).json(err);
+        return res.status(500).json(err);
       }
-      if (data.length < 1) {
-        return res.status(404).end();
+      if (!isMatch) {
+        return res.status(401).end();
       }
-
-      const user = data[0];
-      bcrypt.compare(req.body.password, user.password, function(err, isMatch) {
-        if (err) {
-          return res.status(500).json(err);
-        }
-        if (!isMatch) {
-          return res.status(401).end();
-        }
-        res.status(200).json(user);
-      });
+      res.status(200).json(user);
     });
   });
 });
--- routes/user.js
+++ routes/user.js
@@ -2,69 +2,83 @@ import express from 'express';
 import mysql from 'mysql';
 import bcrypt from 'bcrypt';
 
-const SALT_WORK_FACTOR = 10;
+import Connection from '../constants/Connection';
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Olli9989',
-  database: 'whib',
+const SALT_WORK_FACTOR = 10;
+const user = express.Router();
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(Connection);
+
+  connection.connect(err => {
+    if (err) {
+      console.log(err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log('user router connected');
+    }
+  });
+
+  connection.on('error', err => {
+    console.log(err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+user.route('/').get((req, res) => {
+  connection.query('SELECT * FROM user', (err, data) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    if (data.length < 1) {
+      return res.status(404).end();
+    }
+    res.status(200).json(data);
+  });
 });
 
-const user = express.Router();
-
-connection.connect(err => {
-  if (err) {
-    throw err;
-  }
-  console.log('user router connected');
-
-  user
-    .route('/')
-    .get((req, res) => {
-      connection.query('SELECT * FROM user', (err, data) => {
+user.route('/').post((req, res) => {
+  bcrypt.hash(req.body.password, SALT_WORK_FACTOR, function(err, hash) {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    connection.query(
+      'INSERT INTO user (email, password, username) VALUES (?, ?, ?)',
+      [req.body.email, hash, req.body.username],
+      (err, data) => {
         if (err) {
-          return res.status(404).json(err);
+          return res.status(500).end();
         }
-        res.status(200).json(data);
-      });
-    })
-    .post((req, res) => {
-      bcrypt.hash(req.body.password, SALT_WORK_FACTOR, function(err, hash) {
-        if (err) {
-          return res.status(500).json(err);
-        }
-        connection.query(
-          'INSERT INTO user (email, password, username) VALUES (?, ?, ?)',
-          [req.body.email, hash, req.body.username],
-          (err, data) => {
-            if (err) {
-              return res.status(500).end();
-            }
-            connection.query('SELECT * FROM user WHERE id = ?', [data.insertId], (err, user) => {
-              if (err) {
-                return res.status(500).json(err);
-              }
-              if (user.length < 1) {
-                return res.status(404).end();
-              }
-              res.status(201).json(user[0]);
-            });
-          },
-        );
-      });
-    });
+        connection.query('SELECT * FROM user WHERE id = ?', [data.insertId], (err, user) => {
+          if (err) {
+            return res.status(500).json(err);
+          }
+          if (user.length < 1) {
+            return res.status(404).end();
+          }
+          res.status(201).json(user[0]);
+        });
+      },
+    );
+  });
+});
 
-  user.route('/:id').get((req, res) => {
-    connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, data) => {
-      if (err) {
-        return res.status(500).json(err);
-      }
-      if (data.length < 1) {
-        return res.status(404).end();
-      }
-      res.status(200).json(data[0]);
-    });
+user.route('/:id').get((req, res) => {
+  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, data) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    if (data.length < 1) {
+      return res.status(404).end();
+    }
+    res.status(200).json(data[0]);
   });
 });
 
