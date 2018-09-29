2f65e7661ef3b162954cb5b8a8e9679046670e32
s48024 <s48024@beuth-hochschule.de>
2018-09-29 17:13:22 +0200

    post loactions

--- routes/location.js
+++ routes/location.js
@@ -53,7 +53,7 @@ location.route('/:id').get((req, res) => {
 });
 
 location.route('/:id').post((req, res) => {
-  console.log(req.body);
+  console.log('call');
   const locations = req.body.locations.map(x => {
     const latitude = EarthUtils.getRoundedLatitude(x.latitude);
     const longitude = EarthUtils.getRoundedLongitude(x.longitude, latitude);
