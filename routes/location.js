dc044c9dc497854cc7a703adcf23fd6ca7d8bf58
Oliver Dolgener <oliver.dolgener.wst@codecookies.com>
2018-10-18 14:51:42 +0200

    check level change on insert locations

--- routes/location.js
+++ routes/location.js
@@ -72,7 +72,7 @@ location.route('/:id').post((req, res) => {
       if (err) {
         return res.status(500).json(err);
       }
-      const before = JSON.stringify(data).count;
+      const before = data[0].count;
 
       connection.query(
         'INSERT INTO location (user_id, latitude, longitude, timestamp) VALUES ?',
@@ -89,7 +89,7 @@ location.route('/:id').post((req, res) => {
               if (err) {
                 return res.status(500).json(err);
               }
-              const after = JSON.stringify(data).count;
+              const after = data[0].count;
 
               console.log(before, after);
 
