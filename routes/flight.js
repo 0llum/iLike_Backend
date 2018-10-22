621e78dea92851fdd50dc39b4e5d2114e2f7d4c0
Oliver Dolgener <oliver.dolgener.wst@codecookies.com>
2018-10-22 11:40:11 +0200

    feat(flight): add flight router

--- routes/flight.js
+++ routes/flight.js
@@ -31,7 +31,7 @@ function handleDisconnect() {
 handleDisconnect();
 
 flight.route('/:id').get((req, res) => {
-  connection.query('SELECT start.id as start_id, start.iata_code as start_code, start.latitude as start_latitude, start.longitude as start_longitude, destination.id as destination_id, destination.iata_code as destination_code, destination.latitude as destination_latitude, destination.longitude as destination_longitude FROM `flight` INNER JOIN airport AS start ON start.id = flight.start INNER JOIN airport AS destination ON destination.id = flight.destination WHERE user_id = ?',
+  connection.query('SELECT start.id as start_id, start.iata_code as start_code, start.latitude as start_latitude, start.longitude as start_longitude, destination.id as destination_id, destination.iata_code as destination_code, destination.latitude as destination_latitude, destination.longitude as destination_longitude, timestamp FROM `flight` INNER JOIN airport AS start ON start.id = flight.start INNER JOIN airport AS destination ON destination.id = flight.destination WHERE user_id = ?',
   [req.params.id],
   (err, data) => {
     if (err) {
