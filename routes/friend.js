cdc132995e6220cbe0cfa0477956a87cbea9f8be
0llum <o.dolgener@googlemail.com>
2019-02-02 15:06:27 +0100

    feat(friend): get last tile

--- routes/friend.js
+++ routes/friend.js
@@ -32,14 +32,14 @@ handleDisconnect();
 
 friend.route('/:id').get((req, res) => {
   connection.query(
-    'SELECT user.id, user.username, COUNT(location2.id) AS locations FROM user INNER JOIN friend ON friend.friend_id = user.id INNER JOIN location2 ON location2.user_id = friend.friend_id WHERE friend.user_id = ? GROUP BY location2.user_id ORDER BY user.username',
+    'SELECT user.id, user.username, COUNT(location2.id), user.last_online, user.latitude, user.longitude AS locations FROM user INNER JOIN friend ON friend.friend_id = user.id INNER JOIN location2 ON location2.user_id = friend.friend_id WHERE friend.user_id = ? GROUP BY location2.user_id ORDER BY user.username',
     [req.params.id],
     (err, data) => {
       if (err) {
         return res.status(500).json(err);
       }
       res.status(200).json(data);
-    },
+    }
   );
 });
 
@@ -76,20 +76,20 @@ friend.route('/:id').post((req, res) => {
               }
 
               connection.query(
-                'SELECT user.id, user.username, COUNT(location2.id) AS locations FROM user INNER JOIN friend ON friend.friend_id = user.id INNER JOIN location2 ON location2.user_id = friend.friend_id WHERE friend.user_id = ? GROUP BY location2.user_id ORDER BY user.username',
+                'SELECT user.id, user.username, COUNT(location2.id), user.last_online, user.latitude, user.longitude AS locations FROM user INNER JOIN friend ON friend.friend_id = user.id INNER JOIN location2 ON location2.user_id = friend.friend_id WHERE friend.user_id = ? GROUP BY location2.user_id ORDER BY user.username',
                 [req.params.id],
                 (err, data) => {
                   if (err) {
                     return res.status(500).json(err);
                   }
                   res.status(201).json(data);
-                },
+                }
               );
-            },
+            }
           );
-        },
+        }
       );
-    },
+    }
   );
 });
 
@@ -111,18 +111,18 @@ friend.route('/:id').delete((req, res) => {
           }
 
           connection.query(
-            'SELECT user.id, user.username, COUNT(location2.id) AS locations FROM user INNER JOIN friend ON friend.friend_id = user.id INNER JOIN location2 ON location2.user_id = friend.friend_id WHERE friend.user_id = ? GROUP BY location2.user_id ORDER BY user.username',
+            'SELECT user.id, user.username, COUNT(location2.id), user.last_online, user.latitude, user.longitude AS locations FROM user INNER JOIN friend ON friend.friend_id = user.id INNER JOIN location2 ON location2.user_id = friend.friend_id WHERE friend.user_id = ? GROUP BY location2.user_id ORDER BY user.username',
             [req.params.id],
             (err, data) => {
               if (err) {
                 return res.status(500).json(err);
               }
               res.status(200).json(data);
-            },
+            }
           );
-        },
+        }
       );
-    },
+    }
   );
 });
 
