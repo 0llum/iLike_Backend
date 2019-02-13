02401acb007a18b72380f67cb0459501480207df
Oliver Dolgener <oliver.dolgener@codecookies.com>
2019-02-13 13:49:30 +0100

    fix(world): generate Hennigsdorf

--- routes/world.js
+++ routes/world.js
@@ -6,7 +6,7 @@ import Connection from "../constants/Connection";
 import GeoLocation from "../model/GeoLocation";
 import GeoArray from "../model/GeoArray";
 import * as Earth from "../constants/Earth";
-import Polygon from "../countries/Germany/Brandenburg/Havelland";
+import Polygon from "../countries/Germany/Brandenburg/Oberhavel/Hennigsdorf";
 
 const world = express.Router();
 let connection;
