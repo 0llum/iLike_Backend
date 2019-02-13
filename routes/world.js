d8e87640073ccb754e7fcdd7bf426446b811f14a
Oliver Dolgener <oliver.dolgener@codecookies.com>
2019-02-13 11:38:51 +0100

    fix(world): generate Oberhavel

--- routes/world.js
+++ routes/world.js
@@ -6,7 +6,7 @@ import Connection from '../constants/Connection';
 import GeoLocation from '../model/GeoLocation';
 import GeoArray from '../model/GeoArray';
 import * as Earth from '../constants/Earth';
-import Polygon from '../countries/germany/Oberhavel';
+import Polygon from '../countries/Germany/Brandenburg/Oberhavel';
 
 const world = express.Router();
 let connection;
