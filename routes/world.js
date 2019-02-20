a3691ec057ec752e8c15981015ba8fd17bb39299
Oliver Dolgener <oliver.dolgener@codecookies.com>
2019-02-20 14:18:49 +0100

    generate Mitte

--- routes/world.js
+++ routes/world.js
@@ -6,7 +6,7 @@ import Connection from '../constants/Connection';
 import GeoLocation from '../model/GeoLocation';
 import GeoArray from '../model/GeoArray';
 import * as Earth from '../constants/Earth';
-import Polygon from '../countries/Germany/Berlin/Spandau_16343_AL9.json';
+import Polygon from '../countries/Germany/Berlin/Mitte_16347_AL9.GeoJson';
 
 const world = express.Router();
 let connection;
