0bb335baf52c223ecc5dd9e804fe128dd557160f
Oliver Dolgener <oliver.dolgener@codecookies.com>
2019-02-20 13:49:42 +0100

    test geojson

--- routes/world.js
+++ routes/world.js
@@ -137,7 +137,6 @@ world.route('/generate/:regionId').get((req) => {
 });
 
 const generate = (regionId, polygon, boundingBox, lat = boundingBox.latMax) => {
-  console.log(lat);
   if (lat < boundingBox.latMin) {
     console.log('done');
     return;
@@ -163,9 +162,9 @@ const generate = (regionId, polygon, boundingBox, lat = boundingBox.latMax) => {
     'INSERT INTO world (latitude, longitude, region_id) VALUES ? ON DUPLICATE KEY UPDATE region_id = ?',
     [tiles, regionId],
     (err) => {
-      // if (err) console.log(err);
+      if (err) console.log(err);
       console.log(latitude);
-      generate(regionId, polygon, boundingBox, latitude - Earth.GRID_DISTANCE);
+      // generate(regionId, polygon, boundingBox, latitude - Earth.GRID_DISTANCE);
     },
   );
 };
