0351827e418168729da4edb03aa36cebc42d98c7
Oliver Dolgener <oliver.dolgener@codecookies.com>
2019-02-20 12:15:51 +0100

    test geojson

--- routes/world.js
+++ routes/world.js
@@ -6,7 +6,7 @@ import Connection from '../constants/Connection';
 import GeoLocation from '../model/GeoLocation';
 import GeoArray from '../model/GeoArray';
 import * as Earth from '../constants/Earth';
-import Polygon from '../countries/Germany/Berlin/Spandau_16343_AL9.GeoJson';
+import Polygon from '../countries/Germany/Berlin/Spandau_16343_AL9.json';
 
 const world = express.Router();
 let connection;
