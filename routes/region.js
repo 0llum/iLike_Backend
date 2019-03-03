9086dd804041c7779c400f340b0fe5a8d1f12c03
oliverdolgener <oliver.dolgener@codecookies.com>
2019-03-03 18:37:20 +0100

    generate

--- routes/region.js
+++ routes/region.js
@@ -46,11 +46,37 @@ region.route('/:id').get((req, res) => {
   connection.query(
     'SELECT * FROM region WHERE id = ?',
     [req.params.id],
-    (err, data) => {
+    (err, arr) => {
       if (err) {
         return res.status(500).json(err);
       }
-      res.status(200).json(data);
+
+      const tree = [];
+      const mappedArr = {};
+      let arrElem;
+      let mappedElem;
+
+      for (let i = 0, len = arr.length; i < len; i++) {
+        arrElem = arr[i];
+        mappedArr[arrElem.id] = arrElem;
+        mappedArr[arrElem.id].children = [];
+      }
+
+      for (const id in mappedArr) {
+        if (mappedArr.hasOwnProperty(id)) {
+          mappedElem = mappedArr[id];
+          // If the element is not at the root level, add it to its parent array of children.
+          if (mappedElem.parentid) {
+            mappedArr[mappedElem.parent_id].children.push(mappedElem);
+          }
+          // If the element is at the root level, add it to first level elements array.
+          else {
+            tree.push(mappedElem);
+          }
+        }
+      }
+
+      res.status(200).json(tree);
     },
   );
 });
