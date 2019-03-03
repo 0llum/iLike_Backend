c86ba40dd4b85ad5c0258887f311871ed10d0a7e
oliverdolgener <oliver.dolgener@codecookies.com>
2019-03-03 13:11:55 +0100

    generate

--- routes/world.js
+++ routes/world.js
@@ -6,7 +6,7 @@ import Connection from '../constants/Connection';
 import GeoLocation from '../model/GeoLocation';
 import GeoArray from '../model/GeoArray';
 import * as Earth from '../constants/Earth';
-import Polygon from '../countries/Germany/Berlin/Tempelhof/Friedenau_55752_AL10.json';
+import Polygon from '../countries/Germany/Berlin/Tempelhof/Lichtenrade_157665_AL10.json';
 
 const world = express.Router();
 let connection;
