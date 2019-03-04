c162a5bac0b199dd53995c18f1cf8cc26574ce2a
Oliver Dolgener <oliver.dolgener@codecookies.com>
2019-03-04 15:23:03 +0100

    generate

--- routes/region.js
+++ routes/region.js
@@ -38,25 +38,25 @@ region.route('/').get((req, res) => {
         return res.status(500).json(err);
       }
 
-      const map = {};
-      let node;
-      const roots = [];
-      let i;
+      // const map = {};
+      // let node;
+      // const roots = [];
+      // let i;
 
-      for (i = 0; i < list.length; i += 1) {
-        map[list[i].id] = i;
-        list[i].children = [];
-      }
-      for (i = 0; i < list.length; i += 1) {
-        node = list[i];
-        if (node.parent_id) {
-          list[map[node.parent_id]].children.push(node);
-        } else {
-          roots.push(node);
-        }
-      }
+      // for (i = 0; i < list.length; i += 1) {
+      //   map[list[i].id] = i;
+      //   list[i].children = [];
+      // }
+      // for (i = 0; i < list.length; i += 1) {
+      //   node = list[i];
+      //   if (node.parent_id) {
+      //     list[map[node.parent_id]].children.push(node);
+      //   } else {
+      //     roots.push(node);
+      //   }
+      // }
 
-      res.status(200).json(roots);
+      res.status(200).json(list);
     },
   );
 });
